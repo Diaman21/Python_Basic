6f4484c4509a78535a5a3baf6b6603d3083bd8df
Diaman21 <112503013+Diaman21@users.noreply.github.com>
2023-08-10 14:14:12 +0300

    task_1.py

---
+++ Homework/Homework_5/task_1.py
@@ -0,0 +1,14 @@
+# Напишите функцию, которая принимает на вход строку — абсолютный путь до файла.
+# Функция возвращает кортеж из трёх элементов: путь, имя файла, расширение файла.
+
+def file_path(data):
+    """Функция, которая принимает на вход строку — абсолютный путь до файла.
+    Возвращает кортеж из трёх элементов: путь, имя файла, расширение файла"""
+    data = data.replace(".", " ").replace("\\", " ")
+    *way, name, extension = data.split()
+    res = "\\".join(way), name, extension
+    return res
+
+
+path = "C:\Program Files\Windows Security\BrowserCore\manifest.json"
+print(file_path(path))
